21bf7c2f45f9c6471ce1359eb2140e1112e3153c
Andres Lopez <>
2026-01-20 13:36:14 +0100

    Added deployment pipeline

---
+++ .github/workflows/scripts/DeployToEnv.py
@@ -0,0 +1,32 @@
+import paramiko
+import sys
+import io
+
+build_package=sys.argv[1]
+hostname=sys.argv[2]
+server_path=sys.argv[3]
+ssh_client = paramiko.SSHClient()
+
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+if len(sys.argv)>4:
+    key=sys.argv[4]
+    pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
+    ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
+else:
+    ssh_client.connect(hostname=hostname,port='22',username='ubuntu')
+
+ftp_client=ssh_client.open_sftp()
+ftp_client.put(build_package, build_package)
+
+command = """
+sudo rm -rf """+server_path+"""/* 
+sudo unzip """+build_package+""" -d """+server_path+"""
+sudo systemctl reload nginx
+sleep 10
+"""
+
+stdin, stdout, stderr = ssh_client.exec_command(command)
+for line in stdout.readlines():
+    print(line)
+ftp_client.close()
+ssh_client.close()
