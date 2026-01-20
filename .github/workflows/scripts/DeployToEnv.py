745da352204bc2bcc45c2b99858eccd3d718a523
Andres Lopez <>
2026-01-20 16:00:20 +0100

    Adding more logs to deploy pipeline, missing zip file

--- .github/workflows/scripts/DeployToEnv.py
+++ .github/workflows/scripts/DeployToEnv.py
@@ -7,16 +7,19 @@ hostname=sys.argv[2]
 server_path=sys.argv[3]
 ssh_client = paramiko.SSHClient()
 
-print("Deploying "+build_package+" to "+hostname+":"+server_path)
-
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 if len(sys.argv)>4:
     key=sys.argv[4]
-    print("SSH="+key[0:50]+"...")
     pkey = paramiko.RSAKey.from_private_key(io.StringIO(key))
     ssh_client.connect(hostname=hostname,port='22',username='ubuntu',pkey=pkey)
 else:
     ssh_client.connect(hostname=hostname,port='22',username='ubuntu')
+
+command = """
+echo "UBUNTU: Connected to server"
+ls -la
+"""
+stdin, stdout, stderr = ssh_client.exec_command(command)
 
 ftp_client=ssh_client.open_sftp()
 ftp_client.put(build_package, build_package)
